223e87d4e0cfef7cfbc30cea961f3b0c042ebca0
mohdaquib <mohd.aquib@nagarro.com>
2020-05-28 20:03:49 +0530

    Validate Happy Number

---
+++ src/com/ds/algo/matters/greedy/HappyNumber.java
@@ -0,0 +1,31 @@
+package com.ds.algo.matters.greedy;
+
+public class HappyNumber {
+    public static void main(String[] args){
+        System.out.println(isHappyNumber(15));
+    }
+
+    /** This method will validate number for happy number **/
+    private static boolean isHappyNumber(int n){
+        int slow, fast;
+        slow = fast = n;
+
+        do {
+            slow = numSquareSum(slow);
+            fast = numSquareSum(numSquareSum(fast));
+        } while (slow != fast);
+
+        return slow == 1;
+    }
+
+    /** This method will give sum of squares of digits of number **/
+    private static int numSquareSum(int num){
+        int squareSum = 0;
+        while (num != 0){
+            squareSum += (num%10) * (num%10);
+            num /= 10;
+        }
+
+        return squareSum;
+    }
+}
